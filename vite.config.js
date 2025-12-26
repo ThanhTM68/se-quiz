e2bb753f503a521ca6cdf3f8e358a2cff5f84009
ThanhTM68 <thanhyche68@gmail.com>
2025-12-26 18:38:59 +0700

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
     plugins: [react()],
-    base: "/se-quiz/", // Thay tên repo của bạn vào đây
+    base: "/se-quiz/",
 });
